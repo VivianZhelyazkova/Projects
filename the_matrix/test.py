a0da0e38f53e751d263706ef1b9a1e30ba364cfe
Viviancat666 <vivian9069@gmail.com>
2023-11-20 02:21:11 +0200

    Create test.py

---
+++ the_matrix/test.py
@@ -0,0 +1,18 @@
+import time
+
+first_line = "Wake up, Neo..."
+second_line = "The Matrix has you..."
+third_line = "Follow the white rabbit."
+
+
+def print_line(line):
+    for char in line:
+        print(char, end="", flush=True)
+        time.sleep(0.21)
+    print()
+    time.sleep(1)
+
+
+print_line(first_line)
+print_line(second_line)
+print_line(third_line)
