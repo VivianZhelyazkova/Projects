60f13cf4f5dcf870ce3e9652d814da383028eec2
Viviancat666 <vivian9069@gmail.com>
2023-11-20 03:31:53 +0200

    Update async_test.py

--- the_matrix/async_test.py
+++ the_matrix/async_test.py
@@ -1,16 +1,21 @@
 import requests
-import json
-import datetime
+from datetime import datetime
 
-api_url = "https://api.github.com/users/VivianZhelyazkova"
+username = input()
+api_url = f"https://api.github.com/users/{username}"
 response = requests.get(api_url)
+if response.status_code != 200:
+    print("Username does not exist!")
+    exit()
 response_body = response.json()
 name = response_body["name"]
 email = response_body["email"]
 public_repos = response_body["public_repos"]
 followers = response_body["followers"]
 following = response_body["following"]
-created_at = response_body["created_at"]
+created_at = response_body["created_at"].split("T")[0]
+
+datetime.strptime(created_at, "%Y-%m-%d")
 
 print(f"User info: \n"
       f"Name: {name}\n"
@@ -18,4 +23,4 @@ print(f"User info: \n"
       f"Public repos: {public_repos}\n"
       f"Followers: {followers}\n"
       f"Following: {following}\n"
-      f"Created at: {created_at}\n")
+      f"Created at: {created_at}")
