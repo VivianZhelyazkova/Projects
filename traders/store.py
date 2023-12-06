2bb5f592c250db07fcbebd551cf3bc6a120ccba5
Viviancat666 <vivian9069@gmail.com>
2023-12-07 01:21:03 +0200

    Subclass added

--- traders/store.py
+++ traders/store.py
@@ -3,4 +3,5 @@ class Store:
     def __int__(self, address: str, working_hours: str, area: float, taxes: float):
         self.address = address
         self.working_hours = working_hours
-        self.area = 
+        self.area = area
+        self.taxes = taxes
---
+++ traders/supplier.py
@@ -0,0 +1,15 @@
+from abc import ABC
+
+
+class Supplier(ABC):
+
+    def __init__(self, name: str, address: str, working_hours: str):
+        self.name = name
+        self.address = address
+        self.working_hours = working_hours
+
+
+class WholesaleSupp(Supplier):
+
+    def __init__(self, name: str, address: str, working_hours: str):
+        super().__init__(name, address, working_hours)
