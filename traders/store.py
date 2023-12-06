108dd20927ba35d13af42083b2bf465fd36052ec
Viviancat666 <vivian9069@gmail.com>
2023-12-07 01:07:04 +0200

    Create store.py

---
+++ traders/store.py
@@ -0,0 +1,6 @@
+class Store:
+
+    def __int__(self, address: str, working_hours: str, area: float, taxes: float):
+        self.address = address
+        self.working_hours = working_hours
+        self.area = 
