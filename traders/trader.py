9acabb0cf098c24d5d7a5ccf15d83e9440046f92
Viviancat666 <vivian9069@gmail.com>
2023-12-07 01:03:19 +0200

    Create trader.py

---
+++ traders/trader.py
@@ -0,0 +1,9 @@
+class Trader:
+
+    def __init__(self, name: str, address: str, capital: float, stores: list, suppliers: list):
+        self.name = name
+        self.address = address
+        self.capital = capital
+        self.stores = stores
+        self.suppliers = suppliers
+       
