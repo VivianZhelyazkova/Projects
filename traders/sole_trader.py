501e09f88903e1f317c53732ccbbe4e6471abaac
Viviancat666 <vivian9069@gmail.com>
2023-12-07 01:56:03 +0200

    Update sole_trader.py

--- traders/sole_trader.py
+++ traders/sole_trader.py
@@ -1,3 +1,4 @@
+from Practice.traders.store import Store
 from supplier import RetailSupp
 from trader import Trader
 
@@ -5,4 +6,9 @@ from trader import Trader
 class Sole(Trader):
 
     def __init__(self, name: str, address: str, capital: float, suppliers: list[RetailSupp], stores: list[Store]):
+        if len(suppliers) > 5:
+            raise Exception("Too many suppliers")
+        for supplier in suppliers:
+            if type(supplier) is not RetailSupp:
+                raise Exception("The sole trader only operates with retail suppliers")
         super().__init__(name, address, capital, stores, suppliers)
