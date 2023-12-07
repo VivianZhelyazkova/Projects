d640c723bc10ec5daac7c6840f2cf8beb2dc4268
Viviancat666 <vivian9069@gmail.com>
2023-12-07 02:08:56 +0200

    Update travelling_trader.py

--- traders/travelling_trader.py
+++ traders/travelling_trader.py
@@ -6,9 +6,9 @@ class Travelling(Trader):
 
     def __init__(self, name: str, address: str, capital: float, suppliers: list[RetailSupp]):
         if len(suppliers) > 1:
-            raise Exception("Too many suppliers")
+            raise Exception("Too many suppliers!")
         if type(suppliers[0]) is not RetailSupp:
-            raise Exception("The sole trader only operates with retail suppliers")
+            raise Exception("The sole trader only operates with retail suppliers!")
         super().__init__(name, address, capital, [], suppliers)
 
 
