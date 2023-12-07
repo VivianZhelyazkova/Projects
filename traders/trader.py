97c580d406b326bd316d47bb01adbbd009cae87b
Viviancat666 <vivian9069@gmail.com>
2023-12-07 03:40:18 +0200

    Update trader.py

--- traders/trader.py
+++ traders/trader.py
@@ -11,6 +11,7 @@ class Trader:
         self.capital = capital
         self.stores = stores
         self.suppliers = suppliers
+        self.profit = 0
 
     def order(self, products: list[Product], supplier: Supplier, store: Store):
         discount = supplier.discount
@@ -23,9 +24,25 @@ class Trader:
         else:
             self.capital -= final_sum
             store.add_products(products)
-
+            print("Ordered products:")
+            products = sorted(products, key=lambda x: x.price)
+            for product in products:
+                print(f"name: {product.name}; price: {product.price}")
 
     def collect(self, store: Store):
+        profit = store.sell_products()
+        self.profit += profit
+        print(f"Profit made: {profit:.2f}")
+        available_products = sorted(store.available_products, key=lambda x: x.price)
+        print(f"Available products:")
+        for product in available_products:
+            print(f"name: {product.name}; price: {product.price}")
 
-
-
+    def pay_taxes(self, store: Store):
+        if self.capital >= store.taxes:
+            self.capital -= store.taxes
+        elif self.profit >= store.taxes:
+            self.profit -= store.taxes
+        else:
+            raise Exception("Bankrupt!")
+        print(f"Paid taxes for store on {store.address}: {store.taxes}")
