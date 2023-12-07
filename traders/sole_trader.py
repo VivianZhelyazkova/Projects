c2b8b9a43e5f31e291f9accddcdda67fcde49d92
Viviancat666 <vivian9069@gmail.com>
2023-12-07 02:08:51 +0200

    Update sole_trader.py

--- traders/sole_trader.py
+++ traders/sole_trader.py
@@ -7,12 +7,12 @@ class Sole(Trader):
 
     def __init__(self, name: str, address: str, capital: float, suppliers: list[RetailSupp], stores: list[Store]):
         if len(suppliers) > 5:
-            raise Exception("Too many suppliers")
+            raise Exception("Too many suppliers!")
         for supplier in suppliers:
             if type(supplier) is not RetailSupp:
-                raise Exception("The sole trader only operates with retail suppliers")
+                raise Exception("The sole trader only operates with retail suppliers!")
         if len(stores) > 1:
             raise Exception("Too many stores")
         if type(stores[0]) not in [MarketStall, PushCart]:
-            raise Exception("The sole trader can only have a market stall or a push cart")
+            raise Exception("The sole trader can only have a market stall or a push cart!")
         super().__init__(name, address, capital, stores, suppliers)
